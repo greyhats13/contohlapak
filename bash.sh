9f673ef79f2ac189b5973d63d4a3d3afb63d9978
greyhats13 <imam.arief.rhmn@gmail.com>
2021-10-13 22:51:04 +0700

    query

--- bash.sh
+++ bash.sh
@@ -19,5 +19,5 @@ sudo apt-get -y install docker-compose
 sudo docker build -t contohlapak .
 sudo docker-compose down
 sudo docker-compose up -d
-mysql -u root -ppass -P 3306 -H mysql < tables.sql
+docker exec -i mysql mysql -uroot -ppass < tables.sql
 ./contohlapak
